020e520437a2654b343d33e04b22a2fac99c68a3
Dustin Mitchell <dustin.mitchell@datadoghq.com>
2021-08-09 16:13:01 -0400

    add a tag-line generator

--- bench_test.go
+++ bench_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/djmitche/tagset/tagset"
 )
 
-const dataSize = 1000000
+const dataSize = 10
 
 var data [][]byte
 
@@ -75,26 +75,102 @@ func init() {
 	}
 }
 
+// global places for benchmarks to write, to avoid optimization
+var HashH uint64
+var HashL uint64
+var bySize = map[int]tagLineGenerator{}
+
+func getTLG(size int) tagLineGenerator {
+	if cached, ok := bySize[size]; ok {
+		return cached
+	}
+
+	// almost every line has an environment
+	envs := newLowCardinalityTagGenerator([][]byte{
+		[]byte("env:staging"),
+		[]byte("env:prod"),
+		[]byte("env:dev"),
+		[]byte("env:qa"),
+		[]byte("env:lab"),
+		[]byte("env:bench"),
+		[]byte("env:rainforest"),
+	})
+
+	// some random tags that might or might not appear on a tag line
+	tags := newLowCardinalityTagGenerator([][]byte{
+		[]byte("library:snorkels"),
+		[]byte("library:bunny"),
+		[]byte("library:bananas"),
+		[]byte("library:pamplemousse"),
+		[]byte("abtest:123"),
+		[]byte("abtest:992"),
+		[]byte("abtest:16"),
+		[]byte("shard:x"),
+		[]byte("shard:y"),
+		[]byte("shard:z"),
+	})
+
+	var lowcard tagLineGenerator = newRandomTagLineGenerator([]randomTagLineOptions{{
+		tg:      envs,
+		prob:    99,
+		repeats: 1,
+	}, {
+		tg:      tags,
+		prob:    25,
+		repeats: 3,
+	}})
+
+	// duplicate those, with an active set of 100 tag lines each
+	// repeated up to 20 times
+	lowcard = newDuplicateTagLineGenerator(lowcard, 100, 20)
+
+	spans := newRandomTagLineGenerator([]randomTagLineOptions{{
+		tg:      envs,
+		prob:    99,
+		repeats: 1,
+	}, {
+		tg:      newHighCardinalityTagGenerator("span", size/10),
+		prob:    80,
+		repeats: 1,
+	}})
+
+	var tlg tagLineGenerator = newMultiplexingTagLineGenerator([]tagLineGenerator{
+		lowcard,
+		spans,
+	})
+
+	// lastly, create a preflight generator and preflight it so that
+	// we do not measure tag-line generation time
+	pf := newPreflightTagLineGenerator(size, tlg)
+	pf.preflight()
+
+	bySize[size] = pf
+	return pf
+}
+
 func benchmarkParsing(size int, b *testing.B) {
+	tlg := getTLG(size)
+	hostnames := newHostnameTagGenerator().getTags()
 	foundry := ident.NewInternFoundry()
-	global := tagset.NewWithoutDuplicates([]ident.Ident{foundry.Ident([]byte("planet:earth")), foundry.Ident([]byte("epoch:holocene"))})
-	inc := len(data) / size
+
+	b.ResetTimer()
+
+	global := tagset.NewWithoutDuplicates([]ident.Ident{
+		foundry.Ident([]byte("region:antarctic")),
+		foundry.Ident([]byte("epoch:holocene")),
+	})
 	for i := 0; i < b.N; i++ {
-		common := tagset.DisjointUnion(global, tagset.NewWithoutDuplicates([]ident.Ident{foundry.Ident([]byte(("host:i-1029812")))}))
-		for j := 0; j < len(data); j += inc {
-			tagset.Union(tagset.Parse(foundry, data[i]), common)
+		common := tagset.DisjointUnion(global, tagset.NewWithoutDuplicates([]ident.Ident{
+			foundry.Ident(<-hostnames),
+		}))
+		for line := range tlg.getLines() {
+			ts := tagset.Union(tagset.Parse(foundry, line), common)
+			HashH ^= ts.HashH()
+			HashL ^= ts.HashL()
 		}
 	}
 }
 
-func BenchmarkParsing100(b *testing.B) {
-	benchmarkParsing(100, b)
-}
-
-func BenchmarkParsing10000(b *testing.B) {
-	benchmarkParsing(10000, b)
-}
-
-func BenchmarkParsing1000000(b *testing.B) {
-	benchmarkParsing(1000000, b)
-}
+func BenchmarkParsing1000(b *testing.B)   { benchmarkParsing(1000, b) }
+func BenchmarkParsing10000(b *testing.B)  { benchmarkParsing(10000, b) }
+func BenchmarkParsing100000(b *testing.B) { benchmarkParsing(100000, b) }
