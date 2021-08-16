89641636c0cdbd1fa813f0e8d5cbeabdb049ab24
Dustin J. Mitchell <dustin@v.igoro.us>
2021-08-16 17:43:08 +0000

    begin benchmarks for union operations

--- bench_test.go
+++ bench_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"sync"
 	"testing"
 
@@ -11,12 +12,8 @@ import (
 )
 
 // global places for benchmarks to write, to avoid optimization
-var HashH uint64
-var HashL uint64
 var Line []byte
-
-func init() {
-}
+var TS *tagset.TagSet
 
 // Benchmark reading from a channel
 func BenchmarkChannel(b *testing.B) {
@@ -97,9 +94,7 @@ func benchmarkParsing(b *testing.B, tsFoundry tagset.Foundry) {
 
 		count++
 
-		ts := tsFoundry.Parse(idFoundry, line)
-		HashH ^= ts.HashH()
-		HashL ^= ts.HashL()
+		TS = tsFoundry.Parse(idFoundry, line)
 	}
 
 	b.StopTimer()
@@ -114,3 +109,39 @@ func BenchmarkInternFoundryParsing(b *testing.B) {
 	// report the percent of parses that missed the cache
 	b.ReportMetric(float64(f.ParseMisses)*100/float64(f.Parses), "miss%")
 }
+
+func benchmarkUnion(b *testing.B, tsFoundry tagset.Foundry) {
+	// operate at 1000x the benchmarks, because otherwise allocs/op rounds
+	// to the nearest integer and loses precision
+	n := 1000 * b.N
+	parsingNoteOnce.Do(func() {
+		b.Log("NOTE: for `Benchmark.*FoundryParsing`, one 'op' means parsing 1000 lines.")
+	})
+
+	// get ourselves a collection of tagsets
+	const tagsetCount = 100
+	tagsets := make([]*tagset.TagSet, 0, tagsetCount)
+	tlg := loadgen.NewCmdTagLineGenerator("dsd", tagsetCount)
+	lines := tlg.GetLines()
+	idFoundry := ident.NewInternFoundry()
+	for i := 0; i < tagsetCount; i++ {
+		tagsets = append(tagsets, tsFoundry.Parse(idFoundry, <-lines))
+	}
+
+	test := func(count int) {
+		for i := 0; i < count; i++ {
+			TS = tsFoundry.Union(tagsets[rand.Intn(tagsetCount)], tagsets[rand.Intn(tagsetCount)])
+		}
+	}
+
+	// warm things up a bit..
+	test(1000)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	test(n)
+}
+
+func BenchmarkNullFoundryUnion(b *testing.B)   { benchmarkUnion(b, tagset.NewNullFoundry()) }
+func BenchmarkInternFoundryUnion(b *testing.B) { benchmarkUnion(b, tagset.NewInternFoundry()) }
